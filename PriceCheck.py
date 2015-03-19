3db6caf9835cb358ddc8868c3d6ddbff259251d9
rohitbhalke <rohitbhalke@gmail.com>
2015-03-19 14:47:28 +0530

    Update PriceCheck.py

--- PriceCheck.py
+++ PriceCheck.py
@@ -2,16 +2,20 @@ import urllib2, bs4
 import webbrowser  #for playing song
 import time
 
+
+# enter url of that product which you want to track
 url = "http://www.flipkart.com/forevercarat-adorable-silver-diamond-sterling-plated-ring/p/itmdyz3eq6zugnky?pid=RNGDYZ3EHNJQMTXQ&otracker=from-search&srno=t_6&query=rings&ref=ded8f951-c8d9-4970-904f-342d6cb63090"
 req = urllib2.urlopen(url)
 raw = req.read()
 soup = bs4.BeautifulSoup(raw)
 
-def fetchDataAndCheckPrice(url):
-	objectCamera = soup.find_all("span", class_="selling-price omniture-field")
+myprice= 15000 #enter the price in which you are interested
 
-# extract the prize from span conatining prize in html page 
-	for ob in objectCamera:
+def fetchDataAndCheckPrice(url):
+	objectItem = soup.find_all("span", class_="selling-price omniture-field")
+
+
+	for ob in objectItem:
 		price = ''.join(ob.findAll(text=True))
 
 	price = price[4:].replace(",","") 	
@@ -21,12 +25,12 @@ def fetchDataAndCheckPrice(url):
 
 while 1:
 	price = fetchDataAndCheckPrice(url);
-	if (int(price)<15000):
+	if (int(price)<myprice):
 		print "Yes"
 		webbrowser.open("https://www.youtube.com/watch?v=hfgu9UgRhsc")
 		break;
 	else:
 		print "No"	
-		time.sleep(100)		#if price is not low run this code again after 5 seconds
+		time.sleep(300)		#if price is not low run this code again after 300 seconds
 
 
