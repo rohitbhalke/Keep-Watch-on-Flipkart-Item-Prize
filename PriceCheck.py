54e2052055c96c46d6dd0711f217357ab19f81ee
rohitbhalke <rohitbhalke@gmail.com>
2015-03-26 19:39:09 +0530

    Update PriceCheck.py

--- PriceCheck.py
+++ PriceCheck.py
@@ -4,7 +4,7 @@ import time
 
 
 # enter url of that product which you want to track
-url = "http://www.flipkart.com/forevercarat-adorable-silver-diamond-sterling-plated-ring/p/itmdyz3eq6zugnky?pid=RNGDYZ3EHNJQMTXQ&otracker=from-search&srno=t_6&query=rings&ref=ded8f951-c8d9-4970-904f-342d6cb63090"
+url = "http://www.flipkart.com/rosemary-rm-0038-hand-held-bag/p/itme2fk9x8nd46rs?pid=HMBE2FK9XBANJWYC&srno=b_2&ref=000fa332-69d8-4047-a350-cb1bc15882d9"
 req = urllib2.urlopen(url)
 raw = req.read()
 soup = bs4.BeautifulSoup(raw)
@@ -12,25 +12,35 @@ soup = bs4.BeautifulSoup(raw)
 myprice= 15000 #enter the price in which you are interested
 
 def fetchDataAndCheckPrice(url):
+
 	objectItem = soup.find_all("span", class_="selling-price omniture-field")
 
+	if objectItem:
+		for ob in objectItem:
+			price = ''.join(ob.findAll(text=True))
 
-	for ob in objectItem:
-		price = ''.join(ob.findAll(text=True))
+		price = price[4:].replace(",","") 	
+		print price
 
-	price = price[4:].replace(",","") 	
-	print price
-
-	return price
-
+		return price
+	else:
+		return -1
+		
 while 1:
 	price = fetchDataAndCheckPrice(url);
-	if (int(price)<myprice):
+	if(int(price)==-1):
+		
+		print "Sorry your wish is out of stock :("  #if item is out of stock
+		break	
+	elif(int(price)<myprice):
 		print "Yes"
 		webbrowser.open("https://www.youtube.com/watch?v=hfgu9UgRhsc")
 		break;
 	else:
 		print "No"	
-		time.sleep(300)		#if price is not low run this code again after 300 seconds
+		time.sleep(100)		#if price is not low run this code again after 5 seconds
+	
 
+	
+	
 
